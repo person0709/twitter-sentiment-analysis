b46fbb4fe798942b9a544d4656c1cafdced08e9c
Jeff Yoo <person0709@gmail.com>
2019-06-23 17:33:10 +0900

    gcloud env flex to standard

--- client/src/App.js
+++ client/src/App.js
@@ -51,7 +51,7 @@ class App extends Component {
                         <Row style={{ paddingBottom: '5%' }}>
                             <div>
                                 <h1 style={{ textAlign: 'center' }}>What does Twitter think?</h1>
-                                <div style={{ textAlign: "center", color: "gray" }}>Sentiment analysis provided by Google Cloud NLP</div>
+                                <div style={{ textAlign: "center", color: "gray" }}>Sentiment analysis provided by Google Cloud NLP API</div>
                             </div>
                         </Row>
                     }
@@ -81,7 +81,7 @@ class App extends Component {
                                             magnitude={this.state.magnitude}
                                         />
                                     </Col>
-                                    <span style={{ padding: '10px', marginBottom: '40px' }}>N.B. This is <strong>NOT</strong>
+                                    <span style={{ padding: '10px', marginBottom: '40px' }}>N.B. This is <strong>NOT </strong> 
                                         an entity sentiment analysis, meaning the score is based on the entire tweet 
                                         rather than on the keyword in the context of the tweet. Entity sentiment analysis 
                                         provided by Google Cloud NLP isn't very accurate nor reliable at the moment.
